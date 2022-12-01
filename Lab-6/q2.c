e146dd74f46efc12970ec0bed72ecf8f8dda34bf
payaswini777 <76482976+payaswini777@users.noreply.github.com>
2022-12-01 22:54:42 +0530

    Update q2.c

--- Lab-6/q2.c
+++ Lab-6/q2.c
@@ -2,59 +2,95 @@
 verify the acceptability of the following strings: i. (a,(a,a)) ii. (a,((a,a),(a,a))) You can manually eliminate Left Recursion if any in the gramma*/
 
 
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
+int s(),L(),Ldash();
+char *ip;
+char string[50];
+int main()
+{
+printf("Enter the string\n");
+scanf("%s",string);
+ip=string;
+printf("\n\nInput\tAction\n--------------------------------\n");
 
-#define SIZE 100
-
-char s[SIZE];
-
-int i=0;
-
-void S(), L(), L_();
-
-int main() {
-    printf("Enter a string: ");
-    gets(s);
-    S();
-    if(i==strlen(s)) {
-        printf("\nString parsed.");
-    } else {
-        printf("Error in parsing string.");
-    }
+if(s() && *ip=='\0'){
+printf("\n--------------------------------\n");
+printf("\n String is successfully parsed\n");
+}
+else{
+printf("\n--------------------------------\n");
+printf("Error in parsing String\n");
+}
 }
 
-void S() {
-    if(s[i]=='(') {
-        i++;
-        L();
-        if(s[i]==')') {
-            i++;
+
+int s()
+{
+if(*ip=='(')
+{
+printf("%s\tS->(L) \n",ip);
+ip++;
+if(L())
+{
+if(*ip==')')
+{
+ip++;
+return 1;
+}
+else
+return 0;
+}
+else
+return 0;
+}
+
+else if(*ip=='a')
+{
+ip++;
+printf("%s\tS->a \n",ip);
+return 1;
+}
+else
+return 0;
+}
+
+int L()
+{
+    printf("%s\tL->SL' \n",ip);
+    if(s())
+    {
+    if(Ldash())
+    {
+        return 1;
+    }
+    else
+    return 0;
+    }
+    else
+    return 0;
+}
+int Ldash()
+{
+    if(*ip==',')
+    {
+        ip++;
+        printf("%s\tL'->,SL' \n",ip);
+        if(s())
+        {
+            if(Ldash())
+            {
+                return 1;
+            }
+            else
+            return 0;
         }
-    } else if(s[i]=='a') {
-        i++;
+        else
+        return 0;
+    }
+    else
+    {
+        printf("%s\tL'->^ \n",ip);
+        return 1;
     }
 }
-
-void L() {
-    S();
-    L_();
-}
-
-void L_() {
-    if(s[i]==',') {
-        i++;
-        S();
-        L_();
-    }
-    return;
-}
-
-
-
-/*  S  -> (L)
-    S  -> a
-    L  -> SL'
-    L' -> ,SL'
-    L' -> e
-*/
